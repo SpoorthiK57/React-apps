ebecb71c56eb954248d39b029e223a28fc43dfa2
skamidi55 <kspoorthi.1029@gmail.com>
2025-03-26 09:20:43 -0400

    Added product details page

---
+++ quilling-store/src/context/CartContext.jsx
@@ -0,0 +1,8 @@
+import React, { createContext } from "react";
+
+export const CartContext = createContext(null);
+const CartContext = () => {
+  return <div></div>;
+};
+
+export default CartContext;
--- quilling-store/src/pages/ProductDetails/ProductDetails.jsx
+++ quilling-store/src/pages/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import products from "../../data/product";
+import { useParams } from "react-router-dom";
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const product = products.find((product) => product.id === parseInt(id));
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div className="product-details">
+      <img src={product.image} alt="" />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <button>Add to Cart</button>
+    </div>
+  );
 };
 
 export default ProductDetails;
