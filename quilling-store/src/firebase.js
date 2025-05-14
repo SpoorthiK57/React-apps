6604df6ea7e4b947331fa59a79e78111924d4546
skamidi55 <kspoorthi.1029@gmail.com>
2025-05-14 15:13:31 -0400

    Added firebase authentication

--- quilling-store/src/firebase.js
+++ quilling-store/src/firebase.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgX5X5TrC4CDQmh52Nn7eHWjEBqtJjvJ0",
   authDomain: "quilling-store.firebaseapp.com",
   projectId: "quilling-store",
-  storageBucket: "quilling-store.firebasestorage.app",
+  storageBucket: "quilling-store.appspot.com",
   messagingSenderId: "139815459975",
   appId: "1:139815459975:web:649c7fcd61b36ead55ec22"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const auth = getAuth(app);
+const db = getFirestore(app);
+export {auth,db};
 export default app;
--- quilling-store/src/pages/SignUp/SignUp.jsx
+++ quilling-store/src/pages/SignUp/SignUp.jsx
@@ -1,63 +1,93 @@
 import React, { useState } from "react";
 import "./SignUp.css";
 import { useNavigate } from "react-router-dom";
+import { db, auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const SignUp = () => {
-  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const user = {
-      firstName,
-      lastName,
-      email,
-      password,
-    };
-    localStorage.setItem("user", JSON.stringify(user));
-    navigate("/login");
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      await setDoc(doc(db, "users", user.uid), {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      });
+      console.log("user created:", user);
+      setSuccess(true);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("This email is already registered. Try logging in instead.");
+      } else {
+        alert("Error creating account:" + error.message);
+      }
+    }
   };
 
   return (
     <div className="signup-container">
       <h2>Create Account</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="First Name"
-          placeholder="First Name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-        <input
-          type="Last Name"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
+      {success ? (
+        <div className="success-message">
+          <p>🎉 Account created successfully!</p>
+          <p>
+            <Link to="/login">Click here to login</Link>
+          </p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="First Name"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+          <input
+            type="Last Name"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-        <button type="submit">Create Account</button>
-      </form>
-      <p>
-        Already have an account? <a href="/login">login here</a>
-      </p>
+          <button type="submit">Create Account</button>
+        </form>
+      )}
+      {error && <p className="error-message">{error}</p>}
+
+      {!success && (
+        <p>
+          Already have an account? <Link to="/login">Login here</Link>
+        </p>
+      )}
     </div>
   );
 };
